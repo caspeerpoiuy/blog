7f3c5cf3004fbf68fcd05705d14ee3e55004be2b
yue chen <Yue Chen @yue.chen>
2019-05-06 20:00:27 -0700

    add applications(comment and tools)

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -198,3 +198,15 @@ JWT_AUTH = {
 
 #  specific configuration
 AUTH_USER_MODEL = 'account.CommonUser'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+}
